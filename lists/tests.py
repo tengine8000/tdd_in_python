7e2c101ad461067dc56f50c770e95f361d025e78
Torty Emmanuel <torty.emmanuel@gmail.com>
2020-03-14 18:28:54 +0100

    App handles only one list item.

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.views import home_page
+from lists import home_page
 
 class HomePageTest(TestCase):
 
@@ -11,6 +11,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template (self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
