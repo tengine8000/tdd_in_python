b2dcfc6b8e28e249cef2f957673bfb6e4fcebbc6
Torty Emmanuel <torty.emmanuel@gmail.com>
2020-03-16 14:37:12 +0100

    RESTish APIs completed. Refactored urls to lists app and included.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -109,7 +109,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy Milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('1: Buy Milk')
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
@@ -117,7 +117,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is not trace of Edith's list
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
